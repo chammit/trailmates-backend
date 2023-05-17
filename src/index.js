6bb0ae4d3306021f268bd0233a5fd8f4872ee37d
Clint Hammit <chammit@gmail.com>
2023-05-17 11:03:08 -0500

    Started base routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 3000;
+
+routes(app);
 
 app.get('/', (req, res) => {
 	res.send(`Node and express server is running on port ${PORT}`);
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+const routes = (app) => {
+	app.route('/trails');
+	app.route('/users');
+	app.route('/posts');
+	app.route('/messages');
+};
