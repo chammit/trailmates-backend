08f71a1bb48dab9c6cf700894b8f04c7ecea0c00
Clint Hammit <chammit@gmail.com>
2023-05-19 08:05:13 -0500

    Added basic CRUD endpoints for trails

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,22 @@
+// NOTE: C 5/17/2023 - Initially place get/post/delete type endpoints here before putting each section (trails/users/etc) in separate files
 const routes = (app) => {
-	app.route('/trails');
-	app.route('/users');
-	app.route('/posts');
-	app.route('/messages');
+	app.route('/trails')
+		.get((req, res) => {
+			res.send('GET request sucessful!');
+		})
+		.post((req, res) => {
+			res.send('POST request sucessful!');
+		});
+	app.route('/trails/:trailId')
+		.put((req, res) => {
+			res.send('PUT request sucessful!');
+		})
+		.delete((req, res) => {
+			res.send('DELETE request sucessful!');
+		});
+	// app.route('/users');
+	// app.route('/posts');
+	// app.route('/messages');
 };
+
+module.exports = routes;
