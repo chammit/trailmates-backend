a9ba5e03f383265b27a5d26dc737d1c7c807108b
Clint Hammit <chammit@gmail.com>
2023-05-20 09:22:35 -0500

    Revert "Update index.js"
    This reverts commit 03c84555a4ab8141f0b2c3131196da52611573d2.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
-<<<<<<< Updated upstream
 const express = require('express');
 const routes = require('./routes');
-=======
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
->>>>>>> Stashed changes
 
 const app = express();
 const PORT = 3000;
 
-<<<<<<< Updated upstream
-=======
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Mongoose connection
-mongoose.connect("mongodb://127.0.0.1:27017/trailmates", {
-  useNewUrlParser: true,
-  // useUnifiedTopology: true
-});
-
-// Event handler for successful connection
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
-
-// Event handler for connection error
-mongoose.connection.on("error", (err) => {
-  console.error("Error connecting to MongoDB:", err);
-});
-
->>>>>>> Stashed changes
 routes(app);
 
-app.get("/", (req, res) => {
-  res.send(`Node and express server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+	res.send(`Node and express server is running on port ${PORT}`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Your server is running on port ${PORT}`);
+	console.log(`Your server is running on port ${PORT}`);
 });
